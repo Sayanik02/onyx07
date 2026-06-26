8612b7ca7b6a774baef3a102d76ea6096e8d4114
Sayanik Sarkar <mrshay02@outlook.com>
2026-06-26 11:15:13 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -3,7 +3,7 @@
  * Update ONYX_API_BASE to your server's IP and port.
  * Update ONYX_CLIENT_ID to your Discord bot's application ID.
  */
-window.ONYX_API_BASE  = 'http://legacy-7.hexacraft.fun:25608';   // e.g. http://123.45.67.89:8080
+window.ONYX_API_BASE  = 'https://legacy-7.hexacraft.fun:25608';   // e.g. http://123.45.67.89:8080
 window.ONYX_CLIENT_ID = '1502580749289656341';         // Discord application ID (for invite links)
 
 // ── How to find these values ──────────────────────────────────────────────
